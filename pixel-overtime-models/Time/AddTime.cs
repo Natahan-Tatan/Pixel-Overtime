e0826e89f8f1deba52145b95500bd85cbb375e4d
Natahan <me@natahan.net>
2025-04-11 20:10:54 +0200

    📄 [API] Add license notice into new model files

--- pixel-overtime-models/Time/AddTime.cs
+++ pixel-overtime-models/Time/AddTime.cs
@@ -1,3 +1,34 @@
+//  
+//   Pixel Overtime API
+//   Copyright (C) 2025  Natahan Tatan <license@natahan.net>
+//   
+//   This software is provided free of charge for personal, non-commercial, and non-professional use only.
+//   
+//   Permissions
+//   You are permitted to:
+//   Use this software for personal and educational purposes.
+//   Modify the source code for personal use only.
+//   Share the unmodified version with others for personal use only, as long as this license file is included.
+//   
+//   Restrictions
+//   You are not permitted to:
+//   Use this software in any commercial, professional, or for-profit context.
+//   Sell, sublicense, or distribute this software as part of any paid service or product.
+//   Use this software within an organization or business.
+//   Modify and redistribute the software, unless with the express written permission of the author.
+//   
+//   Disclaimer
+//   This software is provided "as is", without warranty of any kind, express or implied,
+//   including but not limited to the warranties of merchantability, fitness for a particular purpose,
+//   and noninfringement. In no event shall the authors or copyright holders
+//   be liable for any claim, damages or other liability, whether in an action of contract,
+//   tort or otherwise, arising from, out of or in connection with the software or the use
+//   or other dealings in the software.
+//   
+//   ---
+//   
+//   If you wish to use this software for commercial or professional purposes, please contact the author to discuss licensing options.
+
 using System;
 using System.ComponentModel.DataAnnotations;
 
